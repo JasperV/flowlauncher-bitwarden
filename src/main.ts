7b98d8d9976587579dd67800ab18206da09e4678
Jasper <1297464+JasperV@users.noreply.github.com>
2024-07-19 09:51:17 +0200

    add totp to context menu

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { Flow } from "./lib/flow"
 import { z } from "zod"
 import { exec } from 'child_process'
 import childProcess from "child_process"
+import logger from './lib/logger'
 
 const events = ["copy"] as const
 type Events = (typeof events)[number]
@@ -13,13 +14,19 @@ const flow = new Flow<Events>("assets/bitwarden.png")
 flow.on("query", (params) => {
   const [query] = z.array(z.string()).parse(params)
 
-  exec(`bw list items --search ${query} --session ${flow.settings.sessionKey}`, function (error, stdout, stderr) {
-    const results = JSON.parse(stdout).map(result => {
+  if (query.length < 2) return flow.showResult({
+    title: "Searching...",
+    method: "copy"
+  })
+
+  exec(`bw list items --search "${query}" --session "${flow.settings.sessionKey}"`, function (error, stdout, stderr) {
+    const results = JSON.parse(stdout).map((result: any) => {
       return {
         title: result.name,
         subtitle: result.login.username,
         method: "copy",
-        parameters: [result.login.password]
+        parameters: [result.login.password],
+        context: { id: result.id, session: flow.settings.sessionKey }
       }
     })
 
@@ -27,12 +34,23 @@ flow.on("query", (params) => {
   })
 })
 
+flow.on("context_menu", (params) => {
+  const [options] = z.array(z.object({ id: z.string(), session: z.string() })).parse(params)
+
+  exec(`bw get totp "${options.id}" --session "${options.session}"`, function (error, stdout, stderr) {
+    flow.showResult({
+      title: 'Copy TOTP',
+      subtitle: stdout,
+      method: "copy",
+      parameters: [stdout],
+    })
+  })
+})
+
 flow.on("copy", (params) => {
   const [password] = z.array(z.string()).parse(params)
 
-  // exec(`bw get password --itemid ${itemId} --raw --session ${flow.settings.sessionKey}`, function (error, stdout, stderr) {
   childProcess.spawn("clip").stdin?.end(password)
-  // }
 })
 
 flow.run()
